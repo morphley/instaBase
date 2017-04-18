40f0b0f801e3cb249b8d7a71b022ec2ec94daa83
morphley <developer.omari@gmail.com>
2017-04-18 12:08:22 +0200

    Initial Commit

---
+++ instagramFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagramFirebase
+//
+//  Created by Morphley on 18.04.17.
+//  Copyright © 2017 Morphley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
