f31b8c08b98514b0c35db4a4beeec9b8da629160
morphley <developer.omari@gmail.com>
2017-04-18 12:27:29 +0200

    Creating a Registration Screen: Using frame to layout the red view. The problem with using frames when you have devices that are varying in size or the orientation changes  -->  The layout is no longer true -- > The solution is to implement Autolayout.

--- instagramFirebase/ViewController.swift
+++ instagramFirebase/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let plusPhotoButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        return button
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.frame = CGRect(x: 0, y: 0, width: 140, height: 140)
+        plusPhotoButton.center = view.center
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
