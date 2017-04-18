ebdf1cb13cf939bfd0d408a1ca63f82254b9e6ec
morphley <developer.omari@gmail.com>
2017-04-18 20:08:01 +0200

    Creating a Registration Screen: Layout the emailtextfield using auto layout. Next step will be rendering with stack views  is a lot easier.

--- instagramFirebase/ViewController.swift
+++ instagramFirebase/ViewController.swift
@@ -9,35 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     let plusPhotoButton: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        // button.backgroundColor = UIColor.red
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.borderStyle = .roundedRect
+        
+        return tf
+        
+        
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         view.addSubview(plusPhotoButton)
+        view.addSubview(emailTextField)
         // Layout using Frames
-//        plusPhotoButton.frame = CGRect(x: 0, y: 0, width: 140, height: 140)
-//        plusPhotoButton.center = view.center
-
-    
+        //        plusPhotoButton.frame = CGRect(x: 0, y: 0, width: 140, height: 140)
+        //        plusPhotoButton.center = view.center
+        
+        
         
         plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
         
+        
+        emailTextField.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20).isActive = true
+        emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        
+        
     }
-
-
-
-
+    
+    
+    
+    
 }
 
