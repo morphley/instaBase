65f5a973f2699455263220e2805f60117a52e2db
morphley <developer.omari@gmail.com>
2017-04-19 16:12:43 +0200

    User Profile Picture Instagram Firebase
    Now that the necessary Firebase SDKs have been installed and the user authentication step is complete, let's dive into the code that allows more data persistence.  First we'll implement the feature that saves a username into Firebase Database.  Then we move onto using UIImagePickerController to allow the user selection of the profile image in our application.
    
    Lastly, we upload the image into Firebase Storage, which conveniently gives us back a URL for retrieval later.  Finally we take this URL and save it onto our user along with their username.

--- instagramFirebase/ViewController.swift
+++ instagramFirebase/ViewController.swift
@@ -10,15 +10,53 @@ import UIKit
 import Firebase
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
         // button.backgroundColor = UIColor.red
         // button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
+    
+    
+    func  handlePlusPhoto(){
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.allowsEditing = true
+        
+        // set a delegate to imagePickerController
+        present(imagePickerController, animated: true, completion: nil)
+        imagePickerController.delegate = self
+    }
+    
+    
+    
+    // to figure out what image the user pick. Tells the delegate that the user picked a still image or movie.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        
+        // decide between these two images which one whill be shown
+        if  let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        else if  let originalImage = info["UIImagePickerControllerOriginalImage"]  as? UIImage {
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+            
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     let emailTextField: UITextField = {
         let tf = UITextField()
@@ -47,7 +85,7 @@ class ViewController: UIViewController {
             return
         } else {
             // If textfields are empty change color to original
-            signUpButton.isEnabled = false 
+            signUpButton.isEnabled = false
             signUpButton.backgroundColor = UIColor.rgb(red: 149, green: 204, blue: 244)
         }
     }
@@ -61,7 +99,7 @@ class ViewController: UIViewController {
         tf.font = UIFont.systemFont(ofSize: 14)
         tf.borderStyle = .roundedRect
         tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
-
+        
         return tf
     }()
     
@@ -73,7 +111,7 @@ class ViewController: UIViewController {
         tf.font = UIFont.systemFont(ofSize: 14)
         tf.borderStyle = .roundedRect
         tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
-
+        
         tf.isSecureTextEntry = true
         return tf
     }()
@@ -144,9 +182,51 @@ class ViewController: UIViewController {
                 return
                 
             }
+            // use self inside of a completion block
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
             
-            print("Succesfully created user", user?.uid ?? "")
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+            
+            // gives you a random string
+            let fileName = NSUUID().uuidString
+            FIRStorage.storage().reference().child("profile_images").child(fileName).put(uploadData, metadata: nil, completion: { (metadata, err) in
+                
+                if let err = err {
+                
+                print("Failed to upload a profile image", err)
+                    return
+                }
+                
+                guard let profileImageUrl = metadata?.downloadURL()?.absoluteString else { return }
+                
+                print("Succesfully uploaded profile-image", profileImageUrl)
+                
+                
+                
+                // Save the username into Firebase datbase
+                print("Succesfully created user", user?.uid ?? "")
+                
+                // Code that registers the username
+                let dictionaryValues = ["username" : username, "profileImageUrl" : profileImageUrl ]
+                guard let uid = user?.uid else { return }
+                let values = [uid : dictionaryValues]
+                FIRDatabase.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                    if let err = err {
+                        
+                        print("Failed to save userinfo into DB", err)
+                        return
+                    }
+                    print("Succesfully saved userinfo to DB")
+                    
+                })
+
+            })
+            
+            
         })
+        
+        
+        
         
     }
     
