58946aea67826299df408eeaf4c0ad024ada93a4
morphley <developer.omari@gmail.com>
2017-04-22 18:04:40 +0200

    User Photo Selection Instagram Firebase
    In this lesson, we'll be implementing didSelectItem in our UICollectionView to handling the selection of our photos in the list.  In order to show the selected photo, we will setup a custom header that holds reference to a UIImageView that we'll set using our selected image.
    
    In addition to this, we will analyze the behavior of efficiency and UI hanging when trying to optimize for the best user experience.

--- instagramFirebase/PhotoSelectorController.swift
+++ instagramFirebase/PhotoSelectorController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Photos
 
 class PhotoSelectorController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-
+    
     let cellId = "cellId"
     let headerId = "headerId"
     
@@ -18,7 +18,7 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = UIColor.yellow
+        collectionView?.backgroundColor = UIColor.white
         
         
         setupNavigationButtons()
@@ -26,44 +26,87 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
         // register a custom cell
         collectionView?.register(PhotoSelectorCell.self, forCellWithReuseIdentifier: cellId)
         
-        // register a custom header 
-        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        // register a custom header
+        collectionView?.register(PhotoSelectorHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
         
         fetchPhotos()
+   
     }
     
-    var images =  [UIImage]() // empty uiimage array
-    fileprivate func fetchPhotos(){
     
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.selectedImage = images[indexPath.item]
+        self.collectionView?.reloadData() // entire collectionview is going to redraq itself
+    }
+    
+    
+    
+    
+    var selectedImage: UIImage?
+    
+    var images =  [UIImage]() // empty uiimage array
+    var assets = [PHAsset]()
+    
+   
+
+    
+    fileprivate func  assetsFetchOptions() -> PHFetchOptions{
+    
+        
         let fetchOptions = PHFetchOptions()
-        fetchOptions.fetchLimit = 10
+        fetchOptions.fetchLimit = 30
         let sortDiscriptor = NSSortDescriptor(key: "creationDate", ascending: false)
         fetchOptions.sortDescriptors = [sortDiscriptor]
-       let allPhotos =  PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        return fetchOptions
+    
+    }
+    
+    
+    
+    fileprivate func fetchPhotos(){
+   
+        let allPhotos =  PHAsset.fetchAssets(with: .image, options: assetsFetchOptions())
         
-        allPhotos.enumerateObjects({ (asset, count, stop) in
-            print(asset)
+        DispatchQueue.global(qos: .background).async {
             
-            
-            let imageManager = PHImageManager.default()
-            let targetSize = CGSize(width: 350, height: 350)
-            let options = PHImageRequestOptions()
-            options.isSynchronous = true
-            imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { (image, info) in
+            allPhotos.enumerateObjects({ (asset, count, stop) in
+                print(asset)
                 
-                if let image = image {
-                self.images.append(image)
-                }
                 
-                if count == allPhotos.count - 1 {
+                let imageManager = PHImageManager.default()
+                let targetSize = CGSize(width: 200, height: 200)
+                let options = PHImageRequestOptions()
+                options.isSynchronous = true
+                imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { (image, info) in
+                    
+                    if let image = image {
+                        self.images.append(image)
+                        self.assets.append(asset)
+                        
+                        if self.selectedImage == nil {
+                            
+                            self.selectedImage = image  // Anfangsbild setzen
+                        }
+                    }
+                    
+                    if count == allPhotos.count - 1 { // Wenn alle Fotos geladen sind
+                        
+                        DispatchQueue.main.async {
+                            
+                            self.collectionView?.reloadData() // when all photos in the array we call reloadData to modify our cell again
+                            
+                            
+                        }
+                        
+                    }
+                    // print(image)
+                })
                 
-                    self.collectionView?.reloadData() // when all photos in the array we call reloadData to modify our cell again
-                
-                }
-               // print(image)
             })
+
             
-        })
+        }
+        
         
         
     }
@@ -75,7 +118,7 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
         
         return CGSize(width:  width , height: width)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 1
     }
@@ -99,14 +142,44 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
     
     
     
-    // all methods for the header 
+    // all methods for the header
     
     // renders the head
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
-        header.backgroundColor = UIColor.red // you have to provide a size for the header or it wont render
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! PhotoSelectorHeader
+        header.photoImageView.image = selectedImage
+        
+        print(header.photoImageView.image?.size)
+        // header.backgroundColor = UIColor.red // you have to provide a size for the header or it wont render
+       
+        
+        
+       
+        let imageManger = PHImageManager.default()
+        
+        if let selectedImage = selectedImage {
+            if let index = self.images.index(of: selectedImage) {
+                let selectedAsset = self.assets[index]
+                
+                let targetSize = CGSize(width: 600, height: 600)
+                
+                  imageManger.requestImage(for: selectedAsset, targetSize: targetSize, contentMode: .default, options: nil, resultHandler: { (image, info) in
+                    
+                    header.photoImageView.image = image
+                   })
+            
+            
+            }
+            
+            
+        }
+        
+        
+        
+        
+        
         return header
     }
     
@@ -124,31 +197,31 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
     
     
     override var prefersStatusBarHidden: Bool {
-    
-    return true
+        
+        return true
     }
     
     fileprivate func setupNavigationButtons(){
-    
+        
         navigationController?.navigationBar.tintColor = UIColor.black
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
-    
+        
         navigationItem.rightBarButtonItem  = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
-    
-    
+        
+        
     }
     
     
     func handleCancel(){
-    
-    dismiss(animated: true, completion: nil)
+        
+        dismiss(animated: true, completion: nil)
     }
     
     
     func handleNext(){
         
         print(123)
-    
+        
     }
 }
 
---
+++ instagramFirebase/PhotoSelectorHeader.swift
@@ -0,0 +1,40 @@
+//
+//  PhotoSelectorHeader.swift
+//  instagramFirebase
+//
+//  Created by Morphley on 22.04.17.
+//  Copyright © 2017 Morphley. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelectorHeader: UICollectionViewCell{
+    
+    
+    
+    let photoImageView: UIImageView = {
+        
+        let iv = UIImageView()
+        iv.backgroundColor = UIColor.cyan
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+        
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(photoImageView)
+        photoImageView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, bottom: bottomAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 0, paddingBottom: 0, width: 0, height: 0)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+        
+    }
+    
+
+}
