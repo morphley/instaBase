90daf5c750f64b679c10a59f9670ed0ea957d6f8
morphley <developer.omari@gmail.com>
2017-05-31 21:48:05 +0200

    Comments Input Accessory View Instagram Firebase
    Before we can render out our comments inside of the UICollectionView, we'll need to implement a section that contains the input components necessary.  In this lesson, I'll go over a very simple way to implement a keyboard handler by using an inputAccessoryView property.  Using this approach will allow the list above to interact with the input components naturally and easily.

--- instagramFirebase/CommentsController.swift
+++ instagramFirebase/CommentsController.swift
@@ -1,22 +1,64 @@
 //
 //  CommentsController.swift
-//  instagramFirebase
+//  InstagramFirebase
 //
-//  Created by Morphley on 30.05.17.
-//  Copyright © 2017 Morphley. All rights reserved.
+//  Created by Brian Voong on 4/29/17.
+//  Copyright © 2017 Lets Build That App. All rights reserved.
 //
 
 import UIKit
 
-
 class CommentsController: UICollectionViewController {
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Comments"
         
-        collectionView?.backgroundColor = UIColor.red
+        collectionView?.backgroundColor = .red
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    var containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        let submitButton = UIButton(type: .system)
+        submitButton.setTitle("Submit", for: .normal)
+        submitButton.setTitleColor(.black, for: .normal)
+        submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
+        containerView.addSubview(submitButton)
+        submitButton.anchor(top: containerView.topAnchor, left: nil, right: containerView.rightAnchor, bottom: containerView.bottomAnchor, paddingTop: 0, paddingLeft: 0, paddingRight: 12, paddingBottom: 0, width: 50, height: 0)
+        
+        let textField = UITextField()
+        textField.placeholder = "Enter Comment"
+        containerView.addSubview(textField)
+        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, right: submitButton.leftAnchor, bottom: containerView.bottomAnchor, paddingTop: 0, paddingLeft: 12, paddingRight: 0, paddingBottom: 0, width: 0, height: 0)
+        
+        return containerView
+    }()
+    
+    func handleSubmit() {
+        print("Handling Submit...")
+    }
+    
+    override var inputAccessoryView: UIView? {
+        get {
+            return containerView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
     }
 }
